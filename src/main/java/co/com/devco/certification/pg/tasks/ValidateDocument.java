0fa83ac6d7fe32d8f4f22b5a73a6450d19b1d2c2
SimonOtalvaro <75101640+simonotalvaro@users.noreply.github.com>
2023-08-04 13:00:36 -0500

    Subida de cambios

---
+++ src/main/java/co/com/devco/certification/pg/tasks/ValidateDocument.java
@@ -0,0 +1,45 @@
+package co.com.devco.certification.pg.tasks;
+
+import co.com.devco.certification.pg.interactions.ChangeWindows;
+import net.serenitybdd.core.Serenity;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Scroll;
+import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+
+import static co.com.devco.certification.pg.userinterfaces.Page.*;
+
+public class ValidateDocument implements Task {
+
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+
+        actor.attemptsTo(
+                WaitUntil.the(BUTTON_PRODUCTS_SERVICES,WebElementStateMatchers.isEnabled()).forNoMoreThan(5).seconds(),
+                Click.on(BUTTON_PRODUCTS_SERVICES),
+                WaitUntil.the(BUTTON_PRODUCTS,WebElementStateMatchers.isEnabled()).forNoMoreThan(5).seconds(),
+                Click.on(BUTTON_PRODUCTS),
+                Click.on(BUTTON_VIRTUAL_INVERSION),
+                ChangeWindows.number(),
+                Scroll.to(BUTTON_DOCUMENTS).andAlignToTop(),
+                Click.on(BUTTON_DOCUMENTS),
+                Click.on(BUTTON_PDF),
+                ChangeWindows.number()
+        );
+
+        actor.remember("URL",Serenity.getWebdriverManager().getCurrentDriver().getCurrentUrl());
+
+
+    }
+
+    public static ValidateDocument enter(){
+        return Tasks.instrumented(ValidateDocument.class);
+    }
+
+
+}
+
---
+++ src/main/java/co/com/devco/certification/pg/utils/Constant.java
@@ -0,0 +1,7 @@
+package co.com.devco.certification.pg.utils;
+
+public class Constant {
+
+    public static final String URLPDF = "https://www.bancolombia.com/wcm/connect/www.bancolombia.com-26918/182d05c8-d942-42ca-9b3d-ddd0fe8772fe/Reglamento+Inversi%C3%B3n+Virtual.pdf?MOD=AJPERES&CVID=nPHYsMT";
+
+}
